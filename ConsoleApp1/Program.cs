1626722d30aba73ef2f535ea20f133e6a451f017
kingOf <Geshtalt2005@gmail.com>
2020-03-19 19:48:17 +0300

    concole commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.IO;
+
+using Newtonsoft.Json;
 
 namespace ConsoleApplication
 {
@@ -7,12 +10,22 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            var facebookClient = new FacebookClient();
-            var facebookService = new FacebookService(facebookClient);
-            var getAccountTask = facebookService.GetAccountAsync(FacebookSettings.AccessToken);
-            Task.WaitAll(getAccountTask);
-            var account = getAccountTask.Result;
-            Console.WriteLine($"{account.Id} {account.Name}");
+
+
+           
+
+            var cityRaw = File.ReadAllText(@"G:\tf1.json");
+            var city = JsonConvert.DeserializeObject<CityInfo>(cityRaw);
+
+            var t = File.ReadAllText(@"G:\tf4.json");
+            var tt = JsonConvert.DeserializeObject<response>(t);
+
+            //var facebookClient = new FacebookClient();
+            //var facebookService = new FacebookService(facebookClient);
+            //var getAccountTask = facebookService.GetAccountAsync(FacebookSettings.AccessToken);
+            //Task.WaitAll(getAccountTask);
+            //var account = getAccountTask.Result;
+            //Console.WriteLine($"{account.Id} {account.Name}");
 
             //var postOnWallTask = facebookService.PostOnWallAsync(FacebookSettings.AccessToken,
             //"Hello from C# .NET Core!");
---
+++ WebApplication1/Models/FrendInfo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class FrendInfo
+    {
+        
+            public int id { get; set; }
+
+            public string first_name { get; set; }
+
+            public string last_name { get; set; }
+
+            public bool is_closed { get; set; }
+
+            public bool can_access_closed { get; set; }
+
+            public string domain { get; set; }
+
+            public CityInfo city { get; set; }
+
+            public string track_code { get; set; }
+  
+    }
+
+    public class CityInfo
+    {
+
+        public int id { get; set; }
+
+        public string title { get; set; }
+
+    }
+
+
+    public class response
+    {
+        public int count;
+
+        public List<FrendInfo> items;
+    }
+   
+}
